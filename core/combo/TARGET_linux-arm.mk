60cf41fc8f768ce504a5e4860208e078be649b2e
Minux31 <tac.minux@gmail.com>
2014-09-02 16:31:45 +0200

    Using Sabermod toolchain GCC4.8 instead of Google toolchain
    	modifié :         core/combo/TARGET_linux-arm.mk
    
    Change-Id: If5622da1ccf496b70623eb3e40c8abdee1716eaf

--- core/combo/TARGET_linux-arm.mk
+++ core/combo/TARGET_linux-arm.mk
@@ -44,7 +44,9 @@ endif
 # 4.8 is default in my optimizations, as it gives noticable performance boost
 ifeq ($(strip $(TARGET_GCC_VERSION_EXP)),)
 #TARGET_GCC_VERSION := 4.7
-TARGET_GCC_VERSION := 4.8
+#TARGET_GCC_VERSION := 4.8
+# SaberMod Toolchain
+TARGET_GCC_VERSION := 4.8-sm
 else
 TARGET_GCC_VERSION := $(TARGET_GCC_VERSION_EXP)
 endif
